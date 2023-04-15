d83f9b48e03c69bd6838d6a08c499e0435c9da32
Eyobedk <Eyobedkebede10@gmail.com>
2023-04-15 22:37:23 +0000

    InMemory product repository built along with Product model

---
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository() {
+            products = cache["products"] as List<Product>;
+
+            if (products == null) {
+                products = new List<Product>();
+            }
+        }
+    }
+}
